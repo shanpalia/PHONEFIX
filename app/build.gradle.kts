68543b8d2eaa1c784baee802628551bb2852ea29
shanpalia <shanpalia786@gmail.com>
2026-09-14 09:05:20 +0530

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
   alias(libs.plugins.kotlin.compose)
   alias(libs.plugins.google.devtools.ksp)
   alias(libs.plugins.roborazzi)
-  alias(libs.plugins.secrets)
   alias(libs.plugins.google.services)
 }
 
